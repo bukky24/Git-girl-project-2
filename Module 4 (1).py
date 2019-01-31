3932a50842eeda74c48ab170bf2965b0d8823df5
bukky24 <46581093+bukky24@users.noreply.github.com>
2019-01-31 13:58:01 +0100

    Add files via upload

---
+++ Module 4 (1).py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+#Pandas program to get the details of the movie with title 'Grumpier Old Men'
+import pandas as pd
+movies_metadata = pd.read_csv('movies_metadata.csv', index_col =None)
+m = movies_metadata
+Grumpier_Old_Men = m[ m['title'] ==  'Grumpier Old Men']
+Grumpier_Old_Men.head()
+
+
+# In[14]:
+
+# Pandas program to sort the DataFrame based on release_date
+# The Framework only display the following columns: 'title', 'release_date', 'budget', 'revenue', 'runtime'
+movies_metadata = pd.read_csv('movies_metadata.csv', usecols=['title', 'release_date', 'budget', 'revenue', 'runtime'])
+m = movies_metadata
+pm = m.sort_values(by='release_date')
+pm.head(8)
+
+
+# In[5]:
+
+# This program get those movies whose revenue is more than 2 million and spent less than 1 million
+# The framework only display the following columns: 'title', 'release_date', 'budget', 'revenue', 'runtime'
+result = m[(m['revenue'] > 2000000 ) &  (m['budget'] < 1000000) ]
+result.head()
+
+
+# In[ ]:
+
+
+
+
