f63ff2311369f4d783c1871df4d8c34a8a7453c5
bukky24 <46581093+bukky24@users.noreply.github.com>
2019-02-15 11:33:48 +0100

    Add files via upload

--- Unit 2.sql
+++ Unit 2.sql
@@ -1,14 +1,14 @@
 /*What is the total spending (revenue) for each country? */
 
-select BillingCountry as Country, sum(Total) as Revenue
-from invoices 
-group by BillingCountry
-order by Revenue desc
+SELECT BillingCountry as Country, SUM(Total) as Revenue
+FROM invoices 
+GROUP BY BillingCountry
+ORDER BY Revenue DESC
 
 
 /*What is the most prefered Media-type? */
 
-SELECT m.Name as MediaTypeName,m.MediaTypeId, SUM(it.Quantity*it.UnitPrice) as Sales
+SELECT m.Name as MediaTypeName,m.MediaTypeId,COUNT(t.MediaTypeId) as Num, SUM(it.Quantity*it.UnitPrice) as Sales
 FROM media_types m
 JOIN Tracks t
 ON m.MediaTypeId = t.MediaTypeId
@@ -19,3 +19,5 @@ ON it.InvoiceId = i.InvoiceId
 GROUP BY MediaTypeName
 ORDER BY Sales DESC
 LIMIT 1
+
+
